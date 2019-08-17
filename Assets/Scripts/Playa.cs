6dfe6feac11d1fa89872d9a74c9765dc8e3c9e19
jpgcz <jocelinecahoe@gmail.com>
2019-08-17 12:58:31 -0700

    dia uno

---
+++ Assets/Scripts/Playa.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playa : MonoBehaviour
+{
+    [SerializeField, Range(0.1f, 10f)] //Range(-10f, 10f)
+    float moveSpeed;
+
+    void Update()
+    {
+        //Axis es la direccion en que se mueve
+        Vector2 axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.Translate(axis * moveSpeed * Time.deltaTime);
+    }
+}
