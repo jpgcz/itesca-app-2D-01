1c0162af1881da30372f5ba7d8de1f91aed31f65
jpgcz <jocelinecahoe@gmail.com>
2019-08-24 12:59:11 -0700

    tilemap-items2
    Scenario e items

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,6 +6,7 @@ public class Item : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Player")){
+            //Player = Player = other.GetComponent<Player>(); //other.gameObject.GetComponent<Player>();
             Destroy(gameObject);
         }
     }
