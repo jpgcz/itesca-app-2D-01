132ed1fbf1b2078e530e2e909c60cf7490693c04
jpgcz <jocelinecahoe@gmail.com>
2019-08-24 10:38:08 -0700

    animacion
    clase 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField, Range(0.1f, 10f)] //Range(-10f, 10f)
-    float moveSpeed;
+    float moveSpeed = 3f;
+
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    bool moving;
+
+    void Awake(){
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
         //Axis es la direccion en que se mueve
         Vector2 axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.Translate(axis * moveSpeed * Time.deltaTime);
+        moving = axis != Vector2.zero;
+
+        if(moving){
+            animator.SetFloat("move-X", axis.x);
+            animator.SetFloat("move-Y", axis.y);
+        }
+
+        animator.SetBool("moving", moving);
+
+        spriteRenderer.flipX = axis.x < 0 ? true : axis.x > 0 ? false : spriteRenderer.flipX;
     }
 }
